c4936a8cae3af985dff3a5d88495a3c00c23668c
Jiao Hu <43026360+jh6371@users.noreply.github.com>
2020-06-28 17:28:17 -0400

    Create intern.sql

---
+++ Time for Tricks/25.nullintern/intern.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(id) AS number_of_nulls
+FROM departments
+WHERE description IS NULL
+OR UPPER(TRIM(description)) = 'NULL'
+OR UPPER(TRIM(description)) = 'NIL'
+OR UPPER(TRIM(description)) = '-';
